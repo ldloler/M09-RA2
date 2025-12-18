02ac966356faf39fe111db1aab273b0afb7b1c21
ldloler <marc.be.pi@gmail.com>
2025-12-18 19:13:53 +0100

    Optimització del codi :D

--- 02-Futbol/src/Futbolista.java
+++ 02-Futbol/src/Futbolista.java
@@ -3,9 +3,9 @@ public class Futbolista extends Thread {
     private int ngols;
     private int ntirades;
 
-    private final int NUM_JUGADORS = 11;
-    private final int NUM_TIRADES = 20;
-    private final float PROBABILITAT = 0.5f;
+    private final static int NUM_JUGADORS = 11;
+    private final static int NUM_TIRADES = 20;
+    private final static float PROBABILITAT = 0.5f;
 
     public Futbolista(String name) {
         super(name);
@@ -44,46 +44,25 @@ public class Futbolista extends Thread {
     }
 
     public static void main(String[] args) {
-        Futbolista pique = new Futbolista("Piqué");
-        Futbolista vinicius = new Futbolista("Vinicius");
-        Futbolista torres = new Futbolista("Torres");
-        Futbolista ramos = new Futbolista("Ramos");
-        Futbolista ronaldo = new Futbolista("Ronaldo");
-        Futbolista lewan = new Futbolista("Lewan");
-        Futbolista belli = new Futbolista("Belli");
-        Futbolista arnau = new Futbolista("Arnau");
-        Futbolista aspas = new Futbolista("Aspas");
-        Futbolista messi = new Futbolista("Messi");
-        Futbolista mbape = new Futbolista("Mbapé");
+        Futbolista[] futbolistas = new Futbolista[NUM_JUGADORS];
+        String[] nom_jugadors = "Piqué,Vinicius,Torres,Ramos,Ronaldo,Lewan,Belli,Arnau,Aspas,Messi,Mbapé".split(",");
+
+        for (int i = 0; i < futbolistas.length; i++) {
+            futbolistas[i] = new Futbolista(nom_jugadors[i]);
+        }
 
         System.out.println("Inici dels xuts --------------");
-        pique.start();
-        vinicius.start();
-        torres.start();
-        ramos.start();
-        ronaldo.start();
-        lewan.start();
-        belli.start();
-        arnau.start();
-        aspas.start();
-        messi.start();
-        mbape.start();
+        for (Futbolista f : futbolistas) {
+            f.start();
+        }
         System.out.println("Fi dels xuts------------------");
 
         System.out.println("----- Estadistiques ------");
-        System.out.println(
-                String.format("""
-                        %s
-                        %s
-                        %s
-                        %s
-                        %s
-                        %s
-                        %s
-                        %s
-                        %s
-                        %s
-                        %s
-                        """, pique, vinicius, torres, ramos, ronaldo, lewan, belli, arnau, aspas, messi, mbape));
+        StringBuilder sb = new StringBuilder();
+        for (Futbolista f : futbolistas) {
+            sb.append(String.format("%s\n", f));
+        }
+
+        System.out.println(sb.toString());
     }
 }
